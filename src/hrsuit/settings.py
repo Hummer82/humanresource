62f2c80d116cfa0a68cfb9f5794e557961a2b053
Hummer82 <132070793+Hummer82@users.noreply.github.com>
2023-11-26 11:48:29 +0300

    Update settings.py

--- src/hrsuit/settings.py
+++ src/hrsuit/settings.py
@@ -95,13 +95,23 @@ WSGI_APPLICATION = 'hrsuit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hrdb',
+        'USER': 'postgres',
+        'PASSWORD': 'adminpg',
+        'HOST': '127.0.0.1',
+#        'PORT': '7371',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
